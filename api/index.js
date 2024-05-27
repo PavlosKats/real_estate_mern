37d60059fdd696975d9c61df250da3ef0479781f
PavlosKats <paulkatselis@gmail.com>
2024-05-27 09:49:32 +0200

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+// install dotenv to be able to use the .env variable on the backend, then import dotenv and run dotenv.config() to run the server
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('db is connected');
+}).catch((err)=>{
+    console.log(err);
+})
 
 const app = express();
 
