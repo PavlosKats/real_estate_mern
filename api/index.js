9a86fbb90bc5d78b339f9e5d6acb839cc5011308
PavlosKats <paulkatselis@gmail.com>
2024-05-28 10:33:01 +0200

    created and tested signup route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 // install dotenv to be able to use the .env variable on the backend, then import dotenv and run dotenv.config() to run the server
 import dotenv from 'dotenv';
 dotenv.config();
@@ -15,6 +16,9 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 const app = express();
 
+//this is going to allow json as input from the server
+app.use(express.json());
+
 const port = 3000
 
 app.listen(port, ()=>{
@@ -22,3 +26,4 @@ app.listen(port, ()=>{
 })
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
