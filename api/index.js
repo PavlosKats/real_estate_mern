0c4f7fa09ddaf6fabc859237615c749b3a45e144
PavlosKats <paulkatselis@gmail.com>
2024-05-28 09:30:04 +0200

    created and tested api route

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
 // install dotenv to be able to use the .env variable on the backend, then import dotenv and run dotenv.config() to run the server
 import dotenv from 'dotenv';
 dotenv.config();
+
+
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('db is connected');
@@ -17,3 +20,5 @@ const port = 3000
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
 })
+
+app.use("/api/user", userRouter);
