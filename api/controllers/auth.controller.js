69f3db00853f8f0fbcb42a51a1f90773357ea8a4
PavlosKats <paulkatselis@gmail.com>
2024-06-03 08:38:26 +0200

    complete sign up page ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
 
+//create user signup and hash the password with bcrypt
 export const signup = async(req, res, next) => {
     const {username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import {Link} from 'react-router-dom';
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7 text-slate-700'>Sign Up</h1>
+      <form className='flex flex-col gap-5'>
+        <input type="text" className='border p-3 rounded-lg' placeholder='Username' id='username' />
+        <input type="email" className='border p-3 rounded-lg' placeholder='email' id='email' />
+        <input type="password" className='border p-3 rounded-lg' placeholder='password' id='password' />
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
+      </form>
+      <div className="flex gap-3 mt-5">
+        <p>Already have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className='text-blue-700'>Sign In</span> 
+        </Link>
+      </div>
+    </div>
   )
 }
