1319fd40a3921fb05ff665e71f46ac20953806e1
PavlosKats <paulkatselis@gmail.com>
2024-08-05 09:06:51 +0200

    Added google OAuth component

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import {Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector} from 'react-redux';
 import { signInStart, signInFailure, signInSuccess } from '../redux/user/userSlice';
+import OAuth from '../components/OAuth';
+
 
 export default function SignIn() {
 
@@ -61,6 +63,7 @@ export default function SignIn() {
         <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
           {loading ? 'Loading...': 'Sign In'}
         </button>
+        <OAuth/>
       </form>
       <div className="flex gap-3 mt-5" >
         <p>Dont have an account?</p>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import {Link, useNavigate } from 'react-router-dom';
 import { errorHandler } from '../../../api/utils/error';
+import OAuth from '../components/OAuth';
 
 
 export default function SignUp() {
@@ -66,6 +67,7 @@ export default function SignUp() {
         <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
           {loading ? 'Loading...': 'Sign Up'}
         </button>
+        <OAuth/>
       </form>
       <div className="flex gap-3 mt-5" >
         <p>Already have an account?</p>
